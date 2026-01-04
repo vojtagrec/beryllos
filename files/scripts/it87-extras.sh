536b11003a0e83ba25e47ccd3a0d84104342bc4f
Vojta Grec <vojta@grec.dev>
2026-01-04 21:56:47 +0100

    Make it87-extras compilation actually work
    (Also clean up some mess like missing module version.)

--- files/scripts/it87-extras.sh
+++ files/scripts/it87-extras.sh
@@ -4,27 +4,40 @@ set -euxo pipefail
 
 # Compile it87-extras kernel module
 
-KERNEL="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-KVERSION="$(echo "$KERNEL" | cut -d'.' -f1-3)"
+KERNEL="$(uname -r)"
 
-curl -L -O "https://github.com/bazzite-org/kernel-bazzite/releases/download/${KVERSION}/kernel-devel-${KERNEL}.rpm"
-ls -lh "kernel-devel-${KERNEL}.rpm"
-dnf5 -y install "kernel-devel-${KERNEL}.rpm"
+dnf5 -y install "kernel-devel-${KERNEL}"
 dnf5 -y group install development-tools
 
+cp /tmp/beryllos/kmod.priv "/lib/modules/${KERNEL}/build/certs/signing_key.pem"
+cp /tmp/beryllos/kmod.der "/lib/modules/${KERNEL}/build/certs/signing_key.x509"
+
 git clone https://github.com/grandpares/it87.git
-cd it87
+cd it87 || exit 1
 
-make TARGET="$KERNEL" clean
-make TARGET="$KERNEL" modules
+VERSION=$(git show -s --format='%h (%cs)' HEAD)
 
-"/usr/src/kernels/$KERNEL/scripts/sign-file" sha256 /tmp/beryllos/kmod.priv /tmp/beryllos/kmod.der it87-extras.ko
-xz -C crc32 it87-extras.ko
-modinfo "it87-extras.ko.xz" > /dev/null || exit 1
+# Silence warnings caused by broken version detection in grandpares/it87
+touch VERSION
+rm -rf .git
 
-echo 'it87-extras' > it87-extras.conf
+make -C "/lib/modules/${KERNEL}/build" TARGET="$KERNEL" M="$PWD" clean
+make -C "/lib/modules/${KERNEL}/build" \
+    CFLAGS_it87.o="'-DIT87_DRIVER_VERSION=\"${VERSION}\"'" \
+    TARGET="$KERNEL" M="$PWD" modules
 
-mkdir -p /output
-install -Dm644 it87-extras.ko.xz "/output/usr/lib/modules/${KERNEL}/extra/it87-extras/it87-extras.ko.xz"
-install -Dm644 /tmp/beryllos/kmod.der /output/etc/pki/beryllos/certs/kmod.der
-install -Dm644 it87-extras.conf /output/usr/lib/modules-load.d/it87-extras.conf
+modinfo "it87-extras.ko" > /dev/null || exit 1
+
+make -C "/lib/modules/${KERNEL}/build" \
+    INSTALL_MOD_DIR="updates" \
+    CONFIG_MODULE_SIG_KEY="certs/signing_key.pem" \
+    CONFIG_MODULE_COMPRESS_ALL=y \
+    TARGET="$KERNEL" M="$PWD" modules_install
+
+modinfo "/lib/modules/${KERNEL}/updates/it87-extras.ko.xz" \
+    | grep 'sig_key:' | grep -q '37:2C:8D:D7:3B:92:A2:46:09:FA:62:83:C5:8E:F9:FF:5C:E7:B0:B1' || exit 1
+
+#ls -lah "/lib/modules/${KERNEL}" 1>&2
+
+mkdir -p /modules
+cp -rp "/lib/modules/${KERNEL}" "/modules/"
